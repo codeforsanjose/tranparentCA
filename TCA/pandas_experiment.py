a8f03102f6b30e2e9acdd189db249825d02a78aa
Maksim (Max) Habets <Max-Hab@users.noreply.github.com>
2016-03-17 09:50:54 -0700

    tried to use pandas for deduplication

---
+++ TCA/pandas_experiment.py
@@ -0,0 +1,62 @@
+import sys
+import time
+
+import pandas as pd
+
+
+def formated_time(seconds_total):
+    ''' convert number of seconds in the tuple (hours, minutes, seconds)
+    '''
+    minutes, seconds = divmod(seconds_total, 60)
+    hours, minutes = divmod(minutes, 60)
+    seconds = round(seconds)
+    minutes = round(minutes)
+    hours = round(hours)
+
+    return (hours, minutes, seconds)
+
+
+file_start_time = time.time()
+
+data_type = {'Employee Name': str, 'Job Title': str, 'Base Pay': float, 'Overtime Pay': float,
+             'Other Pay': float, 'Benefits': float, 'Total Pay': float,
+             'Total Pay & Benefits': float, 'Year': int, 'Notes': str, 'Agency': str}
+
+error_values = ["Aggregate", "Not Provided", "N/A"]
+
+column_names = ['Employee Name', 'Job Title', 'Base Pay', 'Overtime Pay',
+                'Other Pay', 'Benefits', 'Total Pay',
+                'Total Pay & Benefits', 'Year', 'Notes', 'Agency']
+
+df_payroll = pd.read_csv(
+    '..\\raw_data\\2011-payroll.csv', dtype=data_type, na_values=error_values, header=0, names=column_names)
+
+df_cities = pd.read_csv(
+    '..\\raw_data\\2011-cities.csv', dtype=data_type, na_values=error_values, header=0, names=column_names)
+
+df_counties = pd.read_csv(
+    '..\\raw_data\\2011-counties.csv', dtype=data_type, na_values=error_values, header=0, names=column_names)
+
+df_university_system = pd.read_csv(
+    '..\\raw_data\\2011-university-system.csv', dtype=data_type, na_values=error_values, header=0, names=column_names)
+
+df_result = pd.concat(
+    objs=[df_payroll, df_cities, df_counties, df_university_system])
+file_end_time = time.time()
+print(formated_time(file_end_time - file_start_time))
+print(len(df_result))
+# print(df_result.info())
+df_result.drop_duplicates(['Employee Name', 'Job Title', 'Base Pay', 'Overtime Pay',
+                           'Other Pay', 'Benefits', 'Total Pay',
+                           'Total Pay & Benefits', 'Year'])
+print(len(df_result))
+#df_grouped = df_result.groupby('Job Title').count()
+# df_grouped.to_csv('output.csv')
+df_result.to_csv('output.csv')
+
+''' 
+
+2. memory consuptions
+3. deduplication (see 13.14 in the cookbook)
+
+'''
